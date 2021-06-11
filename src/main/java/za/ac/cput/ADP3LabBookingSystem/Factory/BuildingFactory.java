ade9c21aa2b5a4385a05a43fc206c90875ab2715
Xola Mngeni <xolamngeni@gamil.com>
2021-06-11 18:42:05 +0200

    Building Entity Building Factory Building Factory Test Added comments

--- src/main/java/za/ac/cput/ADP3LabBookingSystem/Factory/BuildingFactory.java
+++ src/main/java/za/ac/cput/ADP3LabBookingSystem/Factory/BuildingFactory.java
@@ -10,7 +10,7 @@ import za.ac.cput.ADP3LabBookingSystem.Entity.Building;
 import za.ac.cput.ADP3LabBookingSystem.Util.GenericHelper;
 
 public class BuildingFactory {
-    public static Building createBuilding(){
+    public static Building createBuilding(String engineering){
         String buildingId = GenericHelper.generateBuildingId();
 
         Building building = new Building.Builder()
