04f545d62766af605871a24f1948ce2dff6036f2
nmkontshwana <214236714@mycput.ac.za>
2021-06-12 02:01:08 +0200

    adding varriables

--- ADP-Assignment-3-Group-15-main/src/test/java/za/ac/cput/ADP3LabBookingSystem/Factory/SeatFactoryTest.java
+++ ADP-Assignment-3-Group-15-main/src/test/java/za/ac/cput/ADP3LabBookingSystem/Factory/SeatFactoryTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SeatFactoryTest {
     @Test
     public void createSeat(){
-        Seat seat = SeatFactory.createSeat("4","yes");
+        Seat seat = SeatFactory.createSeat("4","yes",true);
         System.out.println(seat);
     }
 
